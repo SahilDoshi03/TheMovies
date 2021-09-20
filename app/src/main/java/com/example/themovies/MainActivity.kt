e841bffa18a7fe88db6045cc6f8c784370a714c5
Ishan <Ishan@9029>
2021-09-20 17:34:32 +0530

    Integrated Google, OTP, Email Login and Logout.

--- app/src/main/java/com/example/themovies/MainActivity.kt
+++ app/src/main/java/com/example/themovies/MainActivity.kt
@@ -1,23 +1,47 @@
 package com.example.themovies
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.nav_header.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var googleSignInClient: GoogleSignInClient
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val drawerLayout: DrawerLayout? = findViewById(R.id.drawerLayout)
         val navView: NavigationView? = findViewById(R.id.nav_view)
+        val headerView: View = navView!!.getHeaderView(0)
+        val username = headerView.findViewById<TextView>(R.id.user_name)
+        val email = intent?.getStringExtra("email")
+        val phno = Firebase.auth.currentUser?.phoneNumber
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .build()
+        val sIA: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close)
         toggle.isDrawerIndicatorEnabled = true
@@ -28,18 +52,39 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
-        navView?.setNavigationItemSelectedListener {
+        navView.setNavigationItemSelectedListener {
+
+
+
 
             when(it.itemId){
 
                 R.id.nav_latest -> Toast.makeText(this,"Clicked Latest", Toast.LENGTH_SHORT).show()
                 R.id.nav_toprated -> Toast.makeText(this,"Clicked Top Rated", Toast.LENGTH_SHORT).show()
                 R.id.nav_upcoming -> Toast.makeText(this,"Clicked Upcoming", Toast.LENGTH_SHORT).show()
+                R.id.nav_signIn -> login()
+                R.id.nav_logout -> logout()
             }
             true
         }
 
+        when {
+            email!=null -> {
+                username.text = "$email"
+            }
+            phno!=null -> {
+                username.text = "$phno"
+            }
+            sIA!=null -> {
+                username.setText(sIA.displayName)
+            }
+            else -> {
+                username.text = "Guest"
+            }
+        }
+
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
@@ -48,6 +93,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun logout(){
+        Firebase.auth.signOut()
+        googleSignInClient.signOut().addOnCompleteListener {
+            user_name.text = "Guest"
+        }
+    }
+
+    private fun login() {
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
     }
 
 }
---
+++ app/src/main/java/com/example/themovies/PhNoLogin.kt
@@ -0,0 +1,113 @@
+package com.example.themovies
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.*
+import com.google.firebase.messaging.Constants
+import kotlinx.android.synthetic.main.activity_ph_no_login.*
+import java.util.concurrent.TimeUnit
+
+class PhNoLogin : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+    lateinit var storedVerificationId: String
+    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ph_no_login)
+        auth= FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+        loginPhBtn.setOnClickListener {
+            login()
+        }
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d(Constants.TAG, "onVerificationCompleted:$credential")
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Toast.makeText(applicationContext, "Failed", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+
+                Log.d("TAG","onCodeSent:$verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+            }
+        }
+
+        verifyPhBtn.setOnClickListener {
+            val otp=etOTP.text.toString().trim()
+            if(otp.isNotEmpty()){
+                val credential : PhoneAuthCredential = PhoneAuthProvider.getCredential(storedVerificationId, otp)
+                signInWithPhoneAuthCredential(credential)
+            }else{
+                Toast.makeText(this,"Enter OTP", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun login() {
+        val mobileNumber = findViewById<EditText>(R.id.etPhNo)
+        val prefixno = findViewById<EditText>(R.id.etPhNoPrefix).text.toString().trim()
+        var number = mobileNumber.text.toString().trim()
+
+        if(number.isNotEmpty()){
+            number = prefixno+number
+            sendVerificationCode(number)
+        }else{
+            Toast.makeText(this,
+                "Enter Mobile Number",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendVerificationCode(number: String){
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(number)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                    Toast.makeText(
+                        this,
+                        "You were logged in successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finish()
+// ...
+                } else {
+// Sign in failed, display a message and update the UI
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+// The verification code entered was invalid
+                        Toast.makeText(this,"Invalid OTP",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
+}
